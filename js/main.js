492febbcc3f151363607346d45d3b43c2bffcc3d
yj.j <bab0503@naver.com>
2023-05-08 17:52:45 +0900

    필수선택 수정

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
         let settings = $.extend({
             stateSelector: '.tab-page',
             initialSelector: '.page-initial',
+            page2Selector: '.page2',
             saveSelector : '.page-save',
             finalSelector: '.page-final',
         }, options);
@@ -14,6 +15,12 @@
         let $prev_button = $(settings.prevSelector);
         let $save_button = $('.save-btn');
         let $final_button = $('.final-btn');
+        let page1check = $('.page1check');
+        let page2check = $('.page2check');
+        let page3_1check = $('.page3-1check');
+        let page3_2check = $('.page3-2check');
+        let page4check = $('.page4check');
+
 
         let stateData = $states.map(function () {
             return $(this).hide().data('state');
@@ -25,46 +32,140 @@
         $curr_state.show();
         $prev_button.hide();
 
-        $next_button.click(function () {
-            stateStack.push($curr_state);
-            $prev_button.show();
 
-            let next_state = window[$curr_state.data('evaluator')]();
-            $curr_state.hide(0, function () {
-                $curr_state = $page.getState(next_state);
-                $curr_state.show();
+        page1check.click(function () {
+            let termschk = $('#termschk');
 
-                if ($(settings.saveSelector).is($curr_state))
-                    $next_button.hide(),
-                    $prev_button.hide(),
-                    $save_button.show();
-            });
+            if (termschk.prop('checked') == true) {
+                stateStack.push($curr_state);
+                $prev_button.show();
+
+                let next_state = window[$curr_state.data('evaluator')]();
+                $curr_state.hide(0, function () {
+                    $curr_state = $page.getState(next_state);
+                    $curr_state.show();   
+                });
+            }
         });
 
+        page2check.click(function () {
+            let name = $('#name');
+            let birthDay = $('#birthDay');
+            let hospital = $('#hospital');
+            let pregnancy01 = $('#pregnancy01');
+            let pregnancy02 = $('#pregnancy02');
+
+            if (name.val() !== "") {
+                if (birthDay.val() !== "") {
+                    if (hospital.val() !=="none") {
+                        if(pregnancy01.prop('checked') || pregnancy02.prop('checked') == true){
+
+                            stateStack.push($curr_state);
+
+                            let next_state = window[$curr_state.data('evaluator')]();
+                            $curr_state.hide(0, function () {
+                                $curr_state = $page.getState(next_state);
+                                $curr_state.show();   
+                            });
+                        }
+                    }
+                }
+
+            }
+        });
+
+        page3_1check.click(function () {
+            let gestational = $('#gestational');
+            let lastperiod = $('#lastperiod');
+            let duedate = $('#duedate');
+            let sex01 = $('#sex01');
+            let sex02 = $('#sex02');
+            let cancer01 = $('#cancer01');
+            let cancer02 = $('#cancer02');
+            let inoculation01 = $('#inoculation01');
+            let inoculation02 = $('#inoculation02');
+
+            if(gestational.val() !=="none"){
+                if(lastperiod.val() !== ""){
+                    if(duedate.val() !== ""){
+                        if (sex01.prop('checked') || sex02.prop('checked') == true) {
+                            if(cancer01.prop('checked') || cancer02.prop('checked') == true){
+                                if(inoculation01.prop('checked') || inoculation02.prop('checked') == true){
+                                    
+                                    stateStack.push($curr_state);
+            
+                                    let next_state = window[$curr_state.data('evaluator')]();
+                                    $curr_state.hide(0, function () {
+                                        $curr_state = $page.getState(next_state);
+                                        $curr_state.show();   
+                    
+                                        if ($(settings.saveSelector).is($curr_state))
+                                            $prev_button.hide();
+                                    });
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        });
+
+        page3_2check.click(function () {
+            let menarche01 = $('#menarche01');
+            let menarche02 = $('#menarche02');
+            let cycle = $('#cycle');
+            let monthlyperiod01 = $('#monthlyperiod01');
+            let monthlyperiod02 = $('#monthlyperiod02');
+
+            if (menarche01.prop('checked') ||  menarche02.prop('checked') == true) {
+                if(cycle.val() !=="none"){
+                    if(monthlyperiod01.prop('checked') || monthlyperiod02.prop('checked') == true){
+                        stateStack.push($curr_state);
+
+                        let next_state = window[$curr_state.data('evaluator')]();
+                        $curr_state.hide(0, function () {
+                            $curr_state = $page.getState(next_state);
+                            $curr_state.show();   
+
+                            if ($(settings.saveSelector).is($curr_state))
+                                $prev_button.hide();
+                        });
+                    }
+                }
+            }
+        });
+    
+
+        page4check.click(function () {
+            let itch01 = $('#itch01');
+            let itch02 = $('#itch02');
+
+            if (itch01.prop('checked') || itch02.prop('checked') == true) {
+
+                stateStack.push($curr_state);
+
+                let next_state = window[$curr_state.data('evaluator')]();
+                $curr_state.hide(0, function () {
+                    $curr_state = $page.getState(next_state);
+                    $curr_state.show();   
+                });
+            }
+        });
+
+        
         $prev_button.click(function () {
-            $next_button.show();
 
             let $next_state = stateStack.pop();
             $curr_state.hide(0, function () {
-                $curr_state = $next_state
+                $curr_state = $next_state;
                 $curr_state.show();
-
+            
                 if ($(settings.initialSelector).is($curr_state))
-                    $prev_button.hide();
+                $prev_button.hide();
             });
+
         });
 
-        $save_button.click(function () {
-            stateStack.push($curr_state);
-            $save_button.hide();
-
-            let next_state = window[$curr_state.data('evaluator')]();
-            $curr_state.hide(0, function () {
-                $curr_state = $page.getState(next_state);
-                $curr_state.show();
-                $final_button.show();
-            });
-        });
 
         $page.addClass('fsmachined');
 
@@ -88,6 +189,7 @@
     $.fn.getCurrentState = function () {
         return this.find('.tab-page:visible');
     }
+
 
 
 }(jQuery));
